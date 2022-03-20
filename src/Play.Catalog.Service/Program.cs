d7fe266fa44c97daea59060cfedfb12e183ddb7d
Florent CHANTRET <florent@chantret.com>
2022-03-20 23:24:33 +0100

    Refactor MongoDB registration into extensions methods

--- src/Play.Catalog.Service/Program.cs
+++ src/Play.Catalog.Service/Program.cs
@@ -9,22 +9,10 @@ using Play.Catalog.Service.Settings;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-BsonSerializer.RegisterSerializer(new GuidSerializer(BsonType.String));
-BsonSerializer.RegisterSerializer(new DateTimeOffsetSerializer(BsonType.String));
 ServiceSettings serviceSettings = builder.Configuration.GetSection(nameof(ServiceSettings)).Get<ServiceSettings>();
 
-builder.Services.AddSingleton(serviceProvider =>
-{
-    var mongoDbSettings = builder.Configuration.GetSection(nameof(MongoDbSettings)).Get<MongoDbSettings>();
-    var mongoClient = new MongoClient(mongoDbSettings.ConnectionString);
-    return mongoClient.GetDatabase(serviceSettings.ServiceName);
-});
-
-builder.Services.AddSingleton<IRepository<Item>>(serviceProvider =>
-{
-    var database = serviceProvider.GetService<IMongoDatabase>();
-    return new MongoRepository<Item>(database, "items");
-});
+builder.Services.AddMongo()
+                .AddMongoRepository<Item>("items");
 
 builder.Services.AddControllers(options =>
 {
